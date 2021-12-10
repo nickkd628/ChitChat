7de7b9902d0f89f7c2b9718b6e00bbd6230e9632
nickkd628 <nickkd628@gmail.com>
2021-12-09 21:12:47 -0500

    new features
    Added a edit delete and will update when someone post a new comment

--- firebase.mjs
+++ firebase.mjs
@@ -1,83 +1,105 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
- import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.js";
- import { getFirestore, addDoc, getDocs, collection ,deleteDoc  } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-firestore.js";
- import { getStorage, ref, uploadBytes, getDownloadURL } from"https://www.gstatic.com/firebasejs/9.4.1/firebase-storage.js"
-
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.js";
+import { getFirestore, addDoc, getDocs, collection, deleteDoc, query, orderBy, limit, serverTimestamp, onSnapshot, doc,updateDoc  } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-storage.js";
+// import { query, where, onSnapshot } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-storage.js";
 //TODO MAKE FIREBASE APP ON WEBSITE
+
 const firebaseConfig = {
 
-    apiKey: "AIzaSyDFrJJhaUK1cC7uK9tVRYVwm4v-tp0b--k",
+  apiKey: "AIzaSyDFrJJhaUK1cC7uK9tVRYVwm4v-tp0b--k",
 
-    authDomain: "chitchat-b8d26.firebaseapp.com",
+  authDomain: "chitchat-b8d26.firebaseapp.com",
 
-    projectId: "chitchat-b8d26",
+  projectId: "chitchat-b8d26",
 
-    storageBucket: "chitchat-b8d26.appspot.com",
+  storageBucket: "chitchat-b8d26.appspot.com",
 
-    messagingSenderId: "832890676115",
+  messagingSenderId: "832890676115",
 
-    appId: "1:832890676115:web:7fce26c434d63f4bccfe66"
+  appId: "1:832890676115:web:7fce26c434d63f4bccfe66"
 
-  };
+};
 
 
 
 const app = initializeApp(firebaseConfig);
-  const auth = getAuth();
-  const db = getFirestore(app);
-  const storage = getStorage(app);
+const auth = getAuth();
+const db = getFirestore(app);
+const storage = getStorage(app);
+const latestComment = query(collection(db, "discussions"), orderBy("timestamp", "desc"), limit(1));
 
+window.isLoggedIn = function () {
+  return auth.currentUser !== null;
+}
 
+window.login = function (email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
 
+window.signup = function (email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
 
+window.logout = function () {
+  auth.signOut();
+}
 
-   window.isLoggedIn = function(){
-     return auth.currentUser !== null;
-  }
-
-  window.login = function(email,password){
-      return signInWithEmailAndPassword(auth, email, password);
-  }
-
-  window.signup = function(email,password){
-      return createUserWithEmailAndPassword(auth, email, password);
-  }
-
-  window.logout = function(){
-     auth.signOut();
-  }
-
-  window.onLogin = function( f ){
-     onAuthStateChanged(auth, user => {
-          f( user );
-      });
-  }
+window.onLogin = function (f) {
+  onAuthStateChanged(auth, user => {
+    f(user);
+  });
+}
 
 
 // // //////////////////////////////////////////////
 // // // exposed functionality for db
-  window.addComment = function(comment){
-      return addDoc( collection(db, "discussions"), {comment} );
-  }
-
-
-  window.forEachComment = async function( f ){
-      var docs = await getDocs( collection(db, "discussions") );
-      console.log(docs);
-      docs.forEach( doc => f(doc.data()) );
-  }
-
-  window.uploadFile = function(filename, file){
-    return uploadBytes( ref(storage, filename) , file);
- }
- 
- window.getURL = function(filename){
-  return getDownloadURL( ref(storage, filename) );
-}
-window.deleteDoc = function(comment){
-  return deleteDoc (collection(db,"discussions"), {comment});
+window.addComment = function (comment) {
+  return addDoc(collection(db, "discussions"), { user: auth.currentUser.email, timestamp: serverTimestamp(), comment });
 }
 
 
-//use Snapshot when something changes to update the screen
+window.forEachComment = async function (f) {
+  var docs = await getDocs(collection(db, "discussions"));
+  console.log(docs);
+  docs.forEach(doc => f(doc));
+}
+
+window.uploadFile = function (filename, file) {
+  return uploadBytes(ref(storage, filename), file);
+}
+
+window.getURL = function (filename) {
+  return getDownloadURL(ref(storage, filename));
+}
+
+window.deleteDoc =  function (id) {
+ console.log(id);
+ deleteDoc(doc(db, "discussions", id));
+
+}
+
+// window.startListeningForLatestComment = function( f ){
+//   window.stopListeningForLatestComment = onSnapshot( latestComment, f);
+// }
+
+
+window.addDiscussionEvent = function (f) {
+  // window.removeDiscussionsEvent = onSnapshot(
+  //   query(collection(db, "discussions"), orderBy("timestamp", "desc"), limit(1)),
+  //   querySnapshot =>
+  //     querySnapshot.forEach(doc =>
+  //       f(doc.data(), doc.id)));
+  window.stopListeningForLatestComment = onSnapshot( latestComment,
+    docs => docs.forEach(doc=>{
+       if(doc.data().timestamp)f(doc)}));
+}
+
+ window.edit=function(id,newComment){
+   const updates = doc(db,"discussion",id);
+   updateDoc(updates,{
+     comment: newComment
+
+   });
   
+ }
--- main.js
+++ main.js
@@ -1,104 +1,112 @@
 const $ = document.querySelector.bind(document);
 
-
-
 function createButton(txt) {
-    var btn = document.createElement('button');
-    btn.innerText = txt;
-    $('#button').appendChild(btn);
-    btn.onclick = breed;
-
+  var btn = document.createElement("button");
+  btn.innerText = txt;
+  $("#button").appendChild(btn);
+  btn.onclick = breed;
 }
-
-
 
 function createComment(commentDoc) {
-    var div = document.createElement('div');
-    div.innerText = commentDoc.comment;
-    $('#ChatRooms').appendChild(div);
-    div.className = 'ChatRooms';
+  var div = document.createElement("div");
+  div.innerText = commentDoc.data().comment;
+  $("#ChatRooms").appendChild(div);
+  div.className = "ChatRooms";
+  div.id = commentDoc.id;
+
+
+   div.onmousedown = function (e) {
+     const timeToHoldForDelete = Date.now();
+     div.onmouseup = function (e) {
+       if (Date.now() > timeToHoldForDelete + 3000) {
+        
+         div.remove();
+         deleteDoc(div.id);
+          alert("Deleted");
+       }
+     }
+   }
+
+  div.addEventListener("mouseup", () => {
+    var msg = prompt("are you sure u want to change");
+    if(msg){
+       edit(div.id, msg);
+       div.innerHTML = `${msg}`;
+    }
+   
+  });
 }
 
-
 window.onload = function () {
-
-    // check if user is logged in
-     onLogin(user => {
-        if (user) {
-           //  user just logged in$
-           
-             //added thursday
-             $('#ChatRooms').style.visibility='visible'
-             $('#addComment').style.display = 'block';
-             $('#login').style.display = 'none';
-             $('#signup').style.display = 'none';
-             
-           
-
-         } else {
-          //   user just logged out
-          $('#ChatRooms').style.visibility ='collapse';
-             $('#login').style.display = 'block';
-             $('#addComment').style.display = 'none';
-         }
-     });
-
-
-
-    //show comments
-    forEachComment(createComment);
-
-    ////////////////////////////////
-    // button and link functionality
-    $('#loginLink').onclick = function () {
-        $('#login').style.display = 'block';
-        $('#signup').style.display = 'none';
+  // check if user is logged in
+  onLogin((user) => {
+    if (user) {
+      //  user just logged in$
+      //added thursday
+      $("#ChatRooms").style.visibility = "visible";
+      $("#addComment").style.display = "block";
+      $("#login").style.display = "none";
+      $("#signup").style.display = "none";
+    } else {
+      //   user just logged out
+      $("#ChatRooms").style.visibility = "collapse";
+      $("#login").style.display = "block";
+      $("#addComment").style.display = "none";
     }
+  });
 
-    $('#signupLink').onclick = function () {
-        $('#login').style.display = 'none';
-        $('#signup').style.display = 'block';
-    }
+  //show comments
+  forEachComment(createComment);
 
-    $('#slattbratha').onclick = function () {
-        logout();
-    }
+  ////////////////////////////////
+  // button and link functionality
+  $("#loginLink").onclick = function () {
+    $("#login").style.display = "block";
+    $("#signup").style.display = "none";
+  };
 
-    $('#loginBtn').onclick = function () {
-        login($('#email').value, $('#password').value)
-            .catch(err => $('.error').innerText = err.message);
-    }
+  $("#signupLink").onclick = function () {
+    $("#login").style.display = "none";
+    $("#signup").style.display = "block";
+  };
 
-    $('#registerBtn').onclick = function () {
-        signup($('#emailReg').value, $('#passwordReg').value),$('#users-name').value
-            .catch(err => $('.error').innerText = err.message);
+  $("#slattbratha").onclick = function () {
+    logout();
+  };
 
-    }
+  $("#loginBtn").onclick = function () {
+    login($("#email").value, $("#password").value).catch(
+      (err) => ($(".error").innerText = err.message)
+    );
+  };
 
-    $('#addCommentBtn').onclick = function () {
-        addComment($('#newComment').value)
-            .then(() => {
-                createComment({ comment: $('#newComment').value });
-                $('#newComment').value = '';
-            })
-            .catch(err => $('.error').innerText = err.message)
-    }
-    
-    $('#inputfile').onchange = function(e){
-        uploadFile( this.files[0].name, this.files[0] );
-        // of course you can change this.files[0].name to any filename
-    }
+  $("#registerBtn").onclick = function () {
+    signup($("#emailReg").value, $("#passwordReg").value),
+      $("#users-name").value.catch(
+        (err) => ($(".error").innerText = err.message)
+      );
+  };
 
-    $('#myimage').src = getURL( 'image0.jpg' );
+  $("#addCommentBtn").onclick = function () {
+    addComment($("#newComment").value)
+      .then(() => {
+        createComment({ comment: $("#newComment").value });
+        $("#newComment").value = "";
+      })
+      .catch((err) => ($(".error").innerText = err.message));
+  };
 
-    $('#ChatRooms').addEventListener("click",pressingDown,false) = function(){
-        deleteDoc($('#newComment').value)
-        .then(()=> {
-            createComment({ comment: "" });
-            $('#newComment').value = '';
-        })
-        .catch(err => $('.error').innerText = err.message)
-    }
+  $("#inputfile").onchange = function (e) {
+    uploadFile(this.files[0].name, this.files[0]);
+    // of course you can change this.files[0].name to any filename
+  };
+
+  $("#images").innerHTML = getURL("image0.jpg");
+  getURL("image0.jpg").then((url) => {
+    $("#images").innerHTML = `<img src ="${url}">`;
+  });
+
+  addDiscussionEvent(createComment);
 
 
 }
